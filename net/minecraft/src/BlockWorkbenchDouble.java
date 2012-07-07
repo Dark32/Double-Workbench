492712212d3a728c7e756b5f4b125694b6f7cc46
Vazkii <Vazkii@hotmail.com>
2012-07-07 21:20:05 +0100

    Ported mod to FML/UM2.

--- net/minecraft/src/BlockWorkbenchDouble.java
+++ net/minecraft/src/BlockWorkbenchDouble.java
@@ -1,6 +1,8 @@
 package net.minecraft.src;
 
-public class BlockWorkbenchDouble extends Block {
+import net.minecraft.src.forge.ITextureProvider;
+
+public class BlockWorkbenchDouble extends Block implements ITextureProvider{
 
 	protected BlockWorkbenchDouble(int par1) {
 		super(par1, Material.wood);
@@ -8,15 +10,19 @@ public class BlockWorkbenchDouble extends Block {
 
 	public int getBlockTextureFromSide(int par1)
     {
-    	if(par1 == 0) return 62;
-    	else if (par1 == 1) return mod_DoubleWorkbench.cTTopTex;
-    	else return mod_DoubleWorkbench.cTSideTex;
+    	if(par1 == 0) return 2;
+    	else if (par1 == 1) return 1;
+    	else return 0;
     }
     
     public boolean blockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer)
     {
         	ModLoader.openGUI(par5EntityPlayer, new GuiDoubleCrafting(par5EntityPlayer.inventory, par1World, par2, par3, par4));
             return true;
+    }
+    
+    public String getTextureFile(){
+    	return "/vazkii/doublecrafting/sprites.png";
     }
 
 }
--- net/minecraft/src/mod_DoubleWorkbench.java
+++ net/minecraft/src/mod_DoubleWorkbench.java
@@ -2,16 +2,16 @@ package net.minecraft.src;
 
 import java.util.List;
 
+import vazkii.um.UpdateManagerMod;
+
 import net.minecraft.client.Minecraft;
-import net.minecraft.src.vazkii.updatemanager.IUMAdvanced;
-import net.minecraft.src.vazkii.updatemanager.IUpdateManager;
-import net.minecraft.src.vazkii.updatemanager.ModType;
-import net.minecraft.src.vazkii.updatemanager.UMCore;
 
-public class mod_DoubleWorkbench extends BaseMod implements IUpdateManager, IUMAdvanced{
+public class mod_DoubleWorkbench extends BaseMod{
+	public String getVersion() {
+		return "by Vazkii. Version [1.0.2] for 1.2.5";
+	}
 
-	public mod_DoubleWorkbench() {
-		UMCore.addMod(this);
+	public void load() {
 		ModLoader.registerBlock(doubleWorkbench);
 		ModLoader.addName(doubleWorkbench, "Double Workbench");
 		ModLoader.setInGUIHook(this, true, true);
@@ -19,13 +19,8 @@ public class mod_DoubleWorkbench extends BaseMod implements IUpdateManager, IUMA
         ModLoader.addRecipe(new ItemStack(doubleWorkbench, 1), new Object[] {
             "RGR", "WCW", "SSS", Character.valueOf('S'), Block.cobblestone, Character.valueOf('W'), Block.workbench, Character.valueOf('C'), Block.chest, Character.valueOf('R'), Item.redstone, Character.valueOf('G'), new ItemStack(Item.dyePowder, 1, 8)
         });
-	}
-
-	public String getVersion() {
-		return "by Vazkii. Version [1.0.1] for 1.2.5";
-	}
-
-	public void load() {
+        
+        new UpdateHandler(this);
 	}
 	
     public boolean onTickInGUI(float f, Minecraft minecraft, GuiScreen guiscreen)
@@ -43,30 +38,33 @@ public class mod_DoubleWorkbench extends BaseMod implements IUpdateManager, IUMA
 	@MLProp public static int doubleWorkbenchID = 214;
 	
 	public static final Block doubleWorkbench = (new BlockWorkbenchDouble(doubleWorkbenchID)).setHardness(2.5F).setStepSound(Block.soundWoodFootstep).setBlockName("doubleWorkbench");
-	
-	public static final int cTSideTex = ModLoader.addOverride("/terrain.png", "/vazkii/doublecrafting/dctSide.png");
-	public static final int cTTopTex = ModLoader.addOverride("/terrain.png", "/vazkii/doublecrafting/dctTop.png");
 
 	private static GuiScreen lastGuiOpen;
 
-	public String getModName() {
-		return "Double Workbench";
-	}
-
-	public String getChangelogURL() {
-		return "https://dl.dropbox.com/u/34938401/Mods/On%20Topic/Mods/Double%20Workbench/Changelog.txt";
-	}
-
-	public String getUpdateURL() {
-		return "https://dl.dropbox.com/u/34938401/Mods/On%20Topic/Mods/Double%20Workbench/Version.txt";
-	}
-
-	public String getModURL() {
-		return "http://www.minecraftforum.net/topic/528166-123-mlforge-vazkiis-mods-ebonapi-last-updated-12512/";
-	}
-
-	public ModType getModType() {
-		return ModType.UNDEFINED;
-	}
+	public class UpdateHandler extends UpdateManagerMod{
 	
+		public UpdateHandler(cpw.mods.fml.common.modloader.BaseMod m) {
+			super(m);
+		}
+
+		public String getModName() {
+			return "Double Workbench";
+		}
+
+		public String getChangelogURL() {
+			return "https://dl.dropbox.com/u/34938401/Mods/On%20Topic/Mods/Double%20Workbench/Changelog.txt";
+		}
+
+		public String getUpdateURL() {
+			return "https://dl.dropbox.com/u/34938401/Mods/On%20Topic/Mods/Double%20Workbench/Version.txt";
+		}
+
+		public String getModURL() {
+			return "http://www.minecraftforum.net/topic/528166-123-mlforge-vazkiis-mods-ebonapi-last-updated-12512/";
+		}
+		
+		public String getUMVersion(){
+			return "1.0.2";
+		}
+	}
 }
